5a6ee80412c9bb94958ffdf04f379056fc56d80b
niff8 <juandavidorjuelaibanez@gmail.com>
2023-08-11 06:15:07 -0500

    version234

---
+++ proyecto_juego/Assets/scrips/MuerteCaida.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuerteCaida : MonoBehaviour
+{
+    private Vida vida;
+    //public float cantidadDano;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vida = GetComponent<Vida>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -6){
+            vida.TomarVida(vida.maximoVida);
+        } 
+    }
+}
---
+++ proyecto_juego/Assets/scrips/fruta.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruta : MonoBehaviour
+{
+    
+    //public GameObject efecto;
+    public float cantidadPuntos;
+    public Puntaje puntaje;
+
+    private void OnTriggerEnter2D(Collider2D other){
+
+        if(other.CompareTag("Player"))
+        {
+            //Instantiate(efecto, transform.position, Quaternion.identity);
+            puntaje.SumarPuntos(cantidadPuntos);
+            Destroy(gameObject);
+        }
+    }
+}
